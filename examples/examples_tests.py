317b6f5bc9939839958116e119f012c65df59868
LuchnikovI <luchnikovilya@gmail.com>
2023-09-29 13:52:47 +0200

    examples tesing

---
+++ examples/examples_tests.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from utils import run_sk
+
+def main():
+    print("Testing SK example...")
+    result = run_sk(0.8)
+    assert np.abs(result.bethe_free_entropy - result.replica_symmetric_free_entropy) < 3e-3
+    print(f"Bethe free energy: {result.bethe_free_entropy}, RS free energy: {result.replica_symmetric_free_entropy}")
+    assert result.is_converged
+    print("OK")
+
+if __name__ == "__main__":
+    main()
--- examples/sk_plotter.py
+++ examples/sk_plotter.py
@@ -2,54 +2,9 @@
 
 import os
 import sys
-import subprocess
-from dataclasses import dataclass
-import yaml
 import matplotlib.pyplot as plt  # type: ignore
 import numpy as np
-
-
-@dataclass
-class ExampleResult:
-    is_converged: bool
-    iterations_number: int
-    discrepancy: float
-    bethe_free_entropy: float
-    replica_symmetric_free_entropy: float
-
-"""Runs sk.rs file with its command line parameters passed
-as function arguments"""
-def run_sk(
-        beta: float,
-        spins_number: int = 1000,
-        max_iter: int = 1000,
-        threshold: float = 1e-6,
-        decay: float = 0.5,
-) -> ExampleResult:
-    proc = subprocess.run(
-        f"cargo run --release --example sk -- \
-         --beta {beta} \
-         --spins-number {spins_number} \
-         --max-iter {max_iter} \
-         --threshold {threshold} \
-         --decay {decay}"
-        ,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.DEVNULL,
-        shell=True,
-        check=True,
-    )
-    result_dict = yaml.safe_load(proc.stdout)
-    result = ExampleResult(
-        result_dict["is_converged"],
-        result_dict["iterations_number"],
-        result_dict["discrepancy"],
-        result_dict["bethe_free_entropy"],
-        result_dict["replica_symmetric_free_entropy"],
-    )
-    return result
-
-# -------------------------------------------------------------------------------------------
+from utils import run_sk, ExampleResult
 
 def main():
     betas = np.arange(0., 1.2, 0.05)
